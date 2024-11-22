0009fdfc9de7c77319fc2d2ca2ce6ebce2f18822
William Alexander Aguirre Antolínez <w.alexanderaguirre@gmail.com>
2024-11-22 16:41:40 -0500

    Update Datos consolidados ECV OIME.R

--- Scripts/Importar datos/Datos consolidados ECV OIME.R
+++ Scripts/Importar datos/Datos consolidados ECV OIME.R
@@ -19,6 +19,41 @@ for (i in chunks){
 #Unir los chunks
 data_consolidada<-bind_rows(data_consolidada)
 
+#Asignar el factor de expansión
+factor_expansion<-read.csv2("https://raw.githubusercontent.com/AguirreAntolinez/Arriendos_Medellin/refs/heads/main/Datos/ECV/Data_Consolidada/factor_expansion_personas.csv",header = TRUE,sep = ",")
+
+data_consolidada<-data_consolidada %>% 
+  mutate(
+    GrupoEdad=case_when(
+      Edad <=  4 ~ 1,
+      Edad >= 5 & Edad <=9 ~ 2,
+      Edad >= 10 & Edad <=14 ~ 3,
+      Edad >= 15 & Edad <=19 ~ 4,
+      Edad >= 20 & Edad <=24 ~ 5,
+      Edad >= 25 & Edad <=29 ~ 6,
+      Edad >= 30 & Edad <=34 ~ 7,
+      Edad >= 35 & Edad <=39 ~ 8,
+      Edad >= 40 & Edad <=44 ~ 9,
+      Edad >= 45 & Edad <=49 ~ 10,
+      Edad >= 50 & Edad <=54 ~ 11,
+      Edad >= 55 & Edad <=59 ~ 12,
+      Edad >= 60 & Edad <=64 ~ 13,
+      Edad >= 65 & Edad <=69 ~ 14,
+      Edad >= 70 & Edad <=74 ~ 15,
+      Edad >= 75 & Edad <=79 ~ 16,
+      Edad >= 80  ~ 17),
+    GrupoEdad2=case_when(
+      GrupoEdad==1 | GrupoEdad==2 | GrupoEdad==3 ~ 1,
+      GrupoEdad==4 | GrupoEdad==5 ~ 2,
+      GrupoEdad==6 | GrupoEdad==7 ~ 3,
+      GrupoEdad==8 | GrupoEdad==9 ~ 4,
+      GrupoEdad==10 | GrupoEdad==11 ~ 5,
+      GrupoEdad==12 | GrupoEdad==13 ~ 6,
+      GrupoEdad==14 | GrupoEdad==15 | GrupoEdad==16  | GrupoEdad==17 ~ 7),
+    key=paste0(codigoBarrioComuna,"_",Sexo,"_",GrupoEdad2,"_",medicion)
+    ) %>%
+  left_join(factor_expansion, by = "key")
+
 rentas_depurado<-read.csv2("https://raw.githubusercontent.com/AguirreAntolinez/Arriendos_Medellin/refs/heads/main/Datos/RENTAS%20OIME/rentas_depurado.csv",header = TRUE,sep = ";")
 
 t2<-Sys.time()
